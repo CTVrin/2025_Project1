d73285b16606604d550fdcd5f7d5c7e80f54da02
CTVrin <127947854+CTVrin@users.noreply.github.com>
2025-04-11 15:52:36 +0800

    fixed detect button and now can get player's name

--- reaction.py
+++ reaction.py
@@ -1,16 +1,24 @@
- from gpiozero import LED, Button
- from time import sleep
- from random import uniform
+from gpiozero import LED, Button
+from time import sleep
+from random import uniform
 
- led = LED(4)
+led = LED(4)
+right_button = Button(15)
+left_button = Button(14)
 
+left_name = input('left player's name is: ')
+right_name = input('right player's name is: ')
 
- led.on()
- sleep(uniform(5,10))
- led.off()
+led.on()
+sleep(uniform(5,10))
+led.off()
 
- def pressed(button):
- print(str(button.pin.number) + ' won the game')
+def pressed(button):
+  if button.pin.number == 14:
+   print(left_name + ' won the game')
+  else:
+   print(right_name + ' won the game')
+exit()
 
- right_button.when_pressed = pressed
- left_button.when_pressed = pressed
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed
